e1062df144b90b56126f6a7e5b4767707103907c
megrief <eu.pim@yandex.ru>
2024-05-25 13:18:05 +0500

    Доработан SearchBar. Добавлен debouncer для автоматической отправки запросов.

--- app/src/main/java/com/example/veryinterestingtest/presentation/search/SearchFragment.kt
+++ app/src/main/java/com/example/veryinterestingtest/presentation/search/SearchFragment.kt
@@ -2,17 +2,18 @@ package com.example.veryinterestingtest.presentation.search
 
 import android.os.Bundle
 import android.view.View
+import androidx.core.content.ContextCompat
 import androidx.core.view.isVisible
+import androidx.core.widget.doAfterTextChanged
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
+import com.example.veryinterestingtest.R
 import com.example.veryinterestingtest.databinding.FragmentSearchBinding
 import com.example.veryinterestingtest.presentation.base.BaseFragment
 import com.example.veryinterestingtest.presentation.search.adapter.ImageAdapter
 import com.example.veryinterestingtest.presentation.search.state.SearchScreenState
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class SearchFragment : BaseFragment<FragmentSearchBinding, SearchViewModel>(FragmentSearchBinding::inflate) {
@@ -27,36 +28,47 @@ class SearchFragment : BaseFragment<FragmentSearchBinding, SearchViewModel>(Frag
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
+        with(requireActivity().window) {
+            statusBarColor = ContextCompat.getColor(requireContext(), R.color.elements)
+        }
+        setupSearchField()
         binding.results.adapter = adapter
-        lifecycleScope.launch {
-            withContext(Dispatchers.IO) {
-                viewModel.search("Дерево")
-            }
+        viewLifecycleOwner.lifecycleScope.launch {
 
-            viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
-                viewModel.screenState.collect { state ->
-                    when (state) {
-                        is SearchScreenState.Loading -> {
-                            show(progressBar = true)
-                        }
-                        is SearchScreenState.Results -> {
-                            show(results = true)
-                            (binding.results.adapter as ImageAdapter).submitData(lifecycle, state.images)
-                        }
-                        is SearchScreenState.Error -> {
-                            show(error = true)
-                        }
-                        is SearchScreenState.Empty -> {
-                            show()
-                        }
-                    }
-                }
-            }
+            observeState()
+
         }
         viewModel.screenState
     }
 
+    private fun setupSearchField() {
+        binding.searchField.doAfterTextChanged {
+            viewModel.search(it.toString())
+        }
+    }
+
+    private suspend fun observeState() {
+        viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
+            viewModel.screenState.collect { state ->
+                when (state) {
+                    is SearchScreenState.Loading -> {
+                        show(progressBar = true)
+                    }
+                    is SearchScreenState.Results -> {
+                        show(results = true)
+                        (binding.results.adapter as ImageAdapter).submitData(lifecycle, state.images)
+                    }
+                    is SearchScreenState.Error -> {
+                        show(error = true)
+                        binding.error.text = state.message
+                    }
+                    is SearchScreenState.Empty -> {
+                        show()
+                    }
+                }
+            }
+        }
+    }
     private fun show(
         results: Boolean = false,
         progressBar: Boolean = false,
--- app/src/main/java/com/example/veryinterestingtest/presentation/search/SearchViewModel.kt
+++ app/src/main/java/com/example/veryinterestingtest/presentation/search/SearchViewModel.kt
@@ -4,7 +4,6 @@ import android.content.Context
 import android.net.ConnectivityManager
 import android.net.NetworkCapabilities
 import android.net.Uri
-import android.util.Log
 import androidx.browser.customtabs.CustomTabColorSchemeParams
 import androidx.browser.customtabs.CustomTabsIntent
 import androidx.core.content.ContextCompat
@@ -15,9 +14,12 @@ import com.example.veryinterestingtest.core.entity.SearchQuery
 import com.example.veryinterestingtest.core.usecases.SearchUseCase
 import com.example.veryinterestingtest.presentation.base.BaseViewModel
 import com.example.veryinterestingtest.presentation.search.state.SearchScreenState
+import com.example.veryinterestingtest.util.debounce
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.firstOrNull
+import kotlinx.coroutines.launch
 
 class SearchViewModel(
     private val searchRepo: SearchUseCase,
@@ -27,23 +29,32 @@ class SearchViewModel(
     private val _screenState: MutableStateFlow<SearchScreenState> = MutableStateFlow(SearchScreenState.Empty)
     val screenState: StateFlow<SearchScreenState> = _screenState
 
-    suspend fun search(query: String) {
-        _screenState.value = SearchScreenState.Loading
+    private var lastQuery: String? = null
+
+    private val searchRequest: (String) -> Unit = debounce(SEARCH_DELAY, viewModelScope, true) { query ->
+        if (query.isNotBlank()) {
+            _screenState.value = SearchScreenState.Loading
+            lastQuery = query
+            viewModelScope.launch(Dispatchers.IO) {
+                try {
+                    searchRepo.search(query = SearchQuery(query), page = 1)
+                        .cachedIn(viewModelScope)
+                        .firstOrNull()?.let {
+                            _screenState.value = SearchScreenState.Results(it)
+                        }
+                } catch (e: Exception) {
+                    _screenState.emit(SearchScreenState.Error(e.message?: "Something went wrong"))
+                }
+            }
+        }
+    }
+
+    fun search(query: String) {
         if (!checkInternetConnection()) {
             _screenState.value = SearchScreenState.Error("No internet connection")
             return
         }
-
-        try {
-            Log.wtf("AAAAA", "In search vm")
-            searchRepo.search(query = SearchQuery(query), page = 1)
-                .cachedIn(viewModelScope)
-                .firstOrNull()?.let {
-                    _screenState.value = SearchScreenState.Results(it)
-                }
-        } catch (e: Exception) {
-            _screenState.emit(SearchScreenState.Error(e.message?: "Something went wrong"))
-        }
+        searchRequest(query)
     }
 
     fun openInWeb(context: Context, url: String) {
@@ -69,4 +80,8 @@ class SearchViewModel(
                     || it.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR)
                     || it.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET)
         } ?: false
+
+    companion object {
+        private const val SEARCH_DELAY = 2000L
+    }
 }
